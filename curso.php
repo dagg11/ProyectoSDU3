05fb5dcd95aee6d0ac21d2798341bb990b69a09d
efrencruz <efrenodiloncruzcruz@hotmail.com>
2016-04-19 21:29:50 -0500

    tiene lista de cursos

--- curso.php
+++ curso.php
@@ -1,4 +1,11 @@
-<?php require_once('header.php'); ?>
+<?php 
+session_start();
+require_once('header.php'); 
+require_once('includes/conexion.php');
+
+$sql_curso = "select * from curso";
+$res_curso = mysql_query($sql_curso, $con) or die(mysql_error());
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,35 +16,34 @@
 
 </head>
 <body>
-<br>
-<br>
-<div class="jumbotron">
-  <div class="container text-center">
-    <h1>Curso de ... </h1>
-    <p>Aprende ... desde cero (... capiulos)</p>
-  </div>
-</div>
 
-<div class="container">
-<h2>Información del curso</h2>
-<h3>Primer capitulo Activo</h3>
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th>N°</th>
-        <th>Titulo de capitulo</th>
-        <th>Duracion</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>php</td>
-        <td><a href="cap1html.php">php<a/></td>
-        <td>php</td>
-      </tr>
-      <tr>
-    </tbody>
-  </table>
+<div>
+<br><br><br>
+  <h2>Lista de cursos</h2>
+       
+          <?php while($reg_curso = mysql_fetch_array($res_curso)){ 
+            $id_curso = $reg_curso['id_curso'];
+            ?>
+            <dl>
+            <dt><h3><?php echo $reg_curso['nombre']; ?></h3></dt>
+            <?php 
+            $sql_tuto = "select tutorial.nombre, tutorial.id_tutorial, curso.id_curso from tutorial, curso where tutorial.id_curso = $id_curso and curso.id_curso = $id_curso";
+            $res_tuto = mysql_query($sql_tuto,$con);
+            while($reg_tuto = mysql_fetch_array($res_tuto)){ 
+            ?>  
+            <ul>
+             <li>
+             <?php if($_SESSION){ ?>
+            <a href="verTutorial.php?id_curso=<?php echo $reg_tuto['id_curso'];?>&id_tutorial=<?php echo $reg_tuto['id_tutorial']; ?>"><label> <?php echo $reg_tuto['nombre']; ?> </label></a>
+            <?php } else { ?>
+            <label> <?php echo $reg_tuto['nombre']; ?> </label>
+            <?php } ?>
+            </li> 
+            </ul>
+            <?php } ?>
+
+            </dl>
+          <?php } ?>
 </div>
 
 </body>
